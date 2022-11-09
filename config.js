f90993e108813bb32fa40d5b6d83798880283eda
quangnguyenwx90 <117798986+quangnguyenwx90@users.noreply.github.com>
2022-11-09 12:15:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,15 +6,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'làm người eo teo nhó 🥰',
+    desc: ' phiên tòa này sẽ xét xử m ',
     btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    btnNo: 'Không nha bé  :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu teo <3',
+    btnReply: 'lời nhắn lại   <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/profile.php?id=100037008392536' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
